92a186798bff239b6e4492a1a84b4a9ae08590f5
Coolchickenguy <chicken.science.lab@outlook.com>
2025-03-31 17:58:10 -0600

    Remove java reminants

--- quickcraft-installer/installer.ps1
+++ quickcraft-installer/installer.ps1
@@ -1,5 +1,4 @@
 Remove-Item temp -Force -Recurse -ErrorAction SilentlyContinue
-#Remove-Item java -Force -Recurse -ErrorAction SilentlyContinue
 Remove-Item python -Force -Recurse -ErrorAction SilentlyContinue
 mkdir temp
 $ProgressPreference = 'SilentlyContinue'
@@ -19,9 +18,6 @@ if ($arch -eq "x86") {
 Invoke-WebRequest -OutFile ./temp/python.zip $url
 Expand-Archive temp/python.zip -DestinationPath python
 
-#Invoke-WebRequest -OutFile ./temp/java21.zip https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.6%2B7/OpenJDK21U-jre_x64_windows_hotspot_21.0.6_7.zip
-#Expand-Archive temp/java21.zip -DestinationPath java
-#mv ./java/*/* ./java
 ./python/python ./get-pip.py
 Add-Content -Path ./python/python*._pth -Value 'import site'
 ./python/python -m pip install minecraft_launcher_lib
