7f40dadc3edb0809ee4f7bae826a857c637f3424
Coolchickenguy <chicken.science.lab@outlook.com>
2025-03-31 17:59:33 -0600

    Remove old comments in highlight.mjs

--- highlight.mjs
+++ highlight.mjs
@@ -1,11 +1,7 @@
-// be sure to specify the exact version
 import { codeToHtml } from "https://esm.sh/shiki@3.0.0";
-// or
-// import { codeToHtml } from 'https://esm.run/shiki@3.0.0'
 const handleNode = async (node) => {
   // Handle the newly created node
   console.log("Node created:", node);
-  // Add your logic here, e.g., add event listeners, modify content, etc.
   if (node.nodeName === "CODE") {
     node.innerHTML = await codeToHtml(node.innerText, {
       lang: node.getAttribute("lang"),
