2d4591cbd332c11211dd07fb7743fbbfcb27f658
Coolchickenguy <chicken.science.lab@outlook.com>
2025-04-02 16:43:39 -0600

    Patch for if java was not installed (Happens if you have the offical minecraft launcher then use this andbut you already have the latest version of minecraft installed so it just launches but it never downloaded java so crashes)

--- buildfrom/sources/assets-all/main.py
+++ buildfrom/sources/assets-all/main.py
@@ -1,9 +1,9 @@
 import minecraft_launcher_lib
 import subprocess
-import uuid
 import getpass
 import hashlib
 import os
+import json
 
 ver = minecraft_launcher_lib.utils.get_latest_version()["release"]
 hash=hashlib.sha1(getpass.getuser().encode()).hexdigest()
@@ -12,6 +12,14 @@ minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
 if (not os.path.exists(minecraft_directory)) or list(map(lambda x: x['id'],minecraft_launcher_lib.utils.get_installed_versions(minecraft_directory))).count(ver) == 0:
     print("INSTALLING " + ver)
     minecraft_launcher_lib.install.install_minecraft_version(ver, minecraft_directory)
+
+if not os.path.exists(os.path.join(minecraft_directory,"runtime")):
+    print("Downloading java (You probibly have the offical minecraft launcher and it did not install java to the path we use [your saves will still be here])")
+    with open(os.path.join(minecraft_directory, "versions", ver, ver + ".json"), "r", encoding="utf-8") as f:
+        versiondata = json.load(f)
+        if "javaVersion" in versiondata:
+            minecraft_launcher_lib.runtime.install_jvm_runtime(versiondata["javaVersion"]["component"], minecraft_directory)
+
 print("STARTING " + ver)
 options = {
     "username": uname,
