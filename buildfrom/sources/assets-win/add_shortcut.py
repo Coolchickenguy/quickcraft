3208ebe36c3376b9685c9ddfc7e8b2f55670f340
Coolchickenguy <chicken.science.lab@outlook.com>
2025-04-02 16:06:24 -0600

    add logo to windows shortcut

--- buildfrom/sources/assets-win/add_shortcut.py
+++ buildfrom/sources/assets-win/add_shortcut.py
@@ -1,7 +1,7 @@
 import os
-from win32com.client import Dispatch
+from win32com.client import Dispatch # type: ignore
 
-def create_shortcut(path, target, working_dir=None, arguments=""):
+def create_shortcut(path, target, working_dir=None, arguments="", icon_location=""):
     """
     Creates a Windows shortcut (.lnk) file.
 
@@ -10,12 +10,15 @@ def create_shortcut(path, target, working_dir=None, arguments=""):
         target (str): Path to the target file or folder.
         working_dir (str, optional): Working directory for the shortcut. Defaults to None.
         arguments (str, optional): Arguments to pass to the target. Defaults to "".
+        icon_location (str, optional): The location of the icon for the shortcut. Defaults to "". (Meaning it will resolve to the icon of the target)
     """
     shell = Dispatch('WScript.Shell')
     shortcut = shell.CreateShortCut(path)
     shortcut.Targetpath = target
     shortcut.WorkingDirectory = working_dir if working_dir else os.path.dirname(target)
     shortcut.Arguments = arguments
+    if icon_location is not "":
+        shortcut.IconLocation = icon_location
     shortcut.save()
 
 def get_desktop_path():
@@ -28,5 +31,5 @@ def get_desktop_path():
 if __name__ == '__main__':
     shortcut_path = os.path.join(get_desktop_path(),"quickcraft.lnk")
     target_path = "powershell"
-    create_shortcut(shortcut_path, target_path, working_dir=os.path.realpath("./"), arguments="./start.ps1")
+    create_shortcut(shortcut_path, target_path, working_dir=os.path.realpath("./"), arguments="-ExecutionPolicy Bypass -File ./start.ps1", icon_location=os.path.realpath("./logo.png"))
     print(f"Shortcut created at: {os.path.abspath(shortcut_path)}")
